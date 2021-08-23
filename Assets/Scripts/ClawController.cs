fb9366a5f323d850d8f62c871cad0808ba569592
x674 <roman443@bk.ru>
2021-08-23 14:00:12 +1000

    add control script

---
+++ Assets/Scripts/ClawController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClawController : MonoBehaviour
+{
+    
+    public GameObject Hook,VerticalTube;
+    public Animator ClawAnimator ;
+    [Header("Vertical Distance")] 
+    public float VerticalStartDistance = -0.9833001f;
+    public float VerticalEndDistance = -1.723299f;
+    [Header("Horizontal Distance")] 
+    public float HorizontalStartDistance = -0.9403905f;
+    public float HorizontalEndDistance = 0.8929412f;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (VerticalTube.transform.localPosition.x>VerticalEndDistance)
+            {
+                VerticalTube.transform.Translate(Vector3.back * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (Hook.transform.localPosition.y > HorizontalStartDistance)
+            {
+                Hook.transform.Translate(Vector3.back * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (VerticalTube.transform.localPosition.x<VerticalStartDistance)
+            {
+                VerticalTube.transform.Translate(Vector3.forward * Time.deltaTime);
+            }
+            
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (Hook.transform.localPosition.y < HorizontalEndDistance)
+            {
+                Hook.transform.Translate(Vector3.forward * Time.deltaTime);
+            }
+            
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Drop hook");
+        }
+        
+    }
+}
